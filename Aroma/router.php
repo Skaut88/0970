d38e663d35736a5ee2cd7e3f0fee192097d9ffa6
dkuprin <128187870+Skaut88@users.noreply.github.com>
2023-05-19 15:24:28 +0300

    Add files via upload

---
+++ Aroma/router.php
@@ -0,0 +1,47 @@
+<?php
+
+session_start();
+
+$url = explode("/", $_SERVER['REQUEST_URI']);
+require_once("php/db.php");
+require_once("php/classes/User.php");
+
+if ($url[1] == "blog") {
+  $content = file_get_contents("pages/blog.php");
+} else if ($url[1] == "login") {
+  $content = file_get_contents("pages/login.html");
+} else if ($url[1] == "register") {
+  $content = file_get_contents("pages/register.html");
+} else if ($url[1] == "contact") {
+  $content = file_get_contents("pages/contact.html");
+} else if ($url[1] == "tracking") {
+  $content = file_get_contents("pages/tracking.html");
+} else if ($url[1] == "users") {
+  require_once("pages/users/index.html");
+} else if ($url[1] == "addUser") {
+  echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "authUser"){
+  echo User::authUser($_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "getUser") {
+  echo User::getUser($_SESSION["id"]);
+} else {
+  $content = file_get_contents("pages/index.php");
+}
+
+if (!empty($content))
+  require_once("templates.php");
+
+
+
+
+
+
+// for ($i=0; $i < count($url); $i++) { 
+//   echo $url[$i] . "<hr>";
+// }
+
+// if ($url[1] == "blog") {
+//   require_once("blog.php");
+//   } else if ($url[1] == "auth"){
+//   require_once("login.html");
+//   }
